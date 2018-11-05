4cb9a944d0e7836e74b2e5a5aa56312f8cca236f
Jane Idelson <jane@cactuslab.com>
2018-11-06 09:51:03 +1300

    added listing VC

---
+++ TechTest/ListingsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ListingsViewController.swift
+//  TechTest
+//
+//  Created by Jane Idelson on 5/11/18.
+//  Copyright © 2018 NoName. All rights reserved.
+//
+
+import UIKit
+
+class ListingsViewController: UIViewController {
+    
+    
+    
+}
+//
+//extension ListingsViewController: UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+//    
+//    
+//}
